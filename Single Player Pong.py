1af58abed0715fdef12629cddc336040aab8b23f
Aadiyta Vyas <74606669+AV7coder@users.noreply.github.com>
2020-11-18 09:44:22 +0530

    Add files via upload

---
+++ Single Player Pong.py
@@ -0,0 +1,152 @@
+try:
+   #BY @AV7GAMESTER
+    #COMPLETED :)  
+    from PIL import ImageGrab # for saving
+
+    import turtle
+
+    wn = turtle.Screen()
+    wn.title("Pong game by @AV7GAMESTER")
+    wn.bgcolor("blue")
+    wn.setup(width=800, height=600)
+    wn.tracer(0)
+
+    # Score
+    score_a = 0
+    score_b = 0
+
+
+    # Paddle A
+    paddle_a = turtle.Turtle()
+    paddle_a.speed(0)
+    paddle_a.shape("square")
+    paddle_a.color("red")
+    paddle_a.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_a.penup()
+    paddle_a.goto(-350,0)
+
+    # Paddle B
+    paddle_b = turtle.Turtle()
+    paddle_b.speed(0)
+    paddle_b.shape("square")
+    paddle_b.color("black")
+    paddle_b.shapesize(stretch_wid=5, stretch_len=1)
+    paddle_b.penup()
+    paddle_b.goto(350,0)
+
+
+    #Ball
+    ball = turtle.Turtle()
+    ball.speed(0)
+    ball.shape("circle")
+    ball.color("orange")
+    ball.penup()
+    ball.goto(0,0)
+    ball.dx = 1
+    ball.dy = 1
+
+    # Pen
+    pen = turtle.Turtle()
+    pen.speed(0)
+    pen.color("yellow")
+    pen.penup()
+    pen.hideturtle()
+    pen.goto(0, 260)
+    pen.write("COMPUTER: 0    PLAYER : 0",  align= "center", font=("Courier", 24, "normal"))
+
+    # Function
+    def paddle_a_up():
+        y = paddle_a.ycor()
+        y += 20 
+        paddle_a.sety(y)
+        
+
+    def paddle_a_down():
+        y = paddle_a.ycor()
+        y -= 20 
+        paddle_a.sety(y)
+        
+
+    def paddle_b_down():
+        y = paddle_b.ycor()
+        y -= 20 
+        paddle_b.sety(y)
+
+    def paddle_b_up():
+        y = paddle_b.ycor()
+        y += 20 
+        paddle_b.sety(y)
+
+    def save():
+        bbox = (550, 550, 600, 800) 
+        im = ImageGrab.grab(bbox)
+        im.save('saved_image_pong game.png')
+        
+  
+
+    # Keyboard binding 
+    wn.listen()
+    wn.onkeypress(paddle_b_up, "w")
+    wn.onkeypress(paddle_b_down, "s")
+    wn.onkeypress(paddle_b_down, "Down")
+    wn.onkeypress(paddle_b_up, "Up")
+    wn.onkeypress(save, "s")
+
+    # Main game loop
+    while True:
+        wn.update()
+
+        # Move the ball
+        ball.setx(ball.xcor() + ball.dx)
+        ball.sety(ball.ycor() + ball.dy)
+
+        # Border Checking
+        if ball.ycor() > 290:
+            ball.sety(290)
+            ball.dy *= -1
+            
+
+        if ball.ycor() < -290:
+            ball.sety(-290)
+            ball.dy *= -1    
+
+        if ball.xcor() > 390:
+            ball.goto(0, 0)
+            ball.dx *= -1  
+            score_a += 1 
+            pen.clear()
+            pen.write("COMPUTER: {}    PLAYER : {}".format(score_a, score_b), align= "center", font=("Courier", 24, "normal"))
+        
+        if ball.xcor() < -390:
+            ball.goto(0, 0)
+            ball.dx *= -1
+            score_b += 1
+            pen.clear()
+            pen.write("COMPUTER: {}    PLAYER : {}".format(score_a, score_b), align= "center", font=("Courier", 24, "normal"))
+        
+        # code that makes it singleplayer
+        if paddle_a.ycor() < ball.ycor():
+            if ball.xcor() < -300:
+                paddle_a_up()   
+        if paddle_a.ycor() == ball.ycor():
+            pass
+        if paddle_a.ycor() > ball.ycor():
+            if ball.xcor() < -300:
+                paddle_a_down()   
+        # Paddle and balls collisions
+        if ball.xcor() > 340 and ball.xcor() < 350 and (ball.ycor() < paddle_b.ycor() +40 and ball.ycor() > paddle_b.ycor() -40):
+            ball.setx(340)
+            ball.dx *= -1
+            score_b += 1
+            pen.clear()
+            pen.write("COMPUTER: {}    PLAYER : {}".format(score_a, score_b), align= "center", font=("Courier", 24, "normal"))
+
+
+            
+        if ball.xcor() < -340 and ball.xcor() > -350 and (ball.ycor() < paddle_a.ycor() +40 and ball.ycor() > paddle_a.ycor() -40):
+            ball.setx(-340)
+            ball.dx *= -1
+        # else:
+        #     pass
+except:
+    print("App can't start..... Please Retry")
